e56b99c74b47e19d7825d5110bbe3e1b77f6372a
mcollins <fixitman.mike@gmail.com>
2022-06-09 20:18:49 -0400

    added auth tracking and remember me

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout'
@@ -8,31 +8,37 @@ import Login from './pages/Login';
 import RequireAuth from './components/RequireAuth';
 import UserLists from './pages/UserLists';
 import ListContents from './pages/ListContents';
+import { useStoreActions } from 'easy-peasy';
 
 
 function App() {
 
+  const subscribeToAuthChanges = useStoreActions(actions => actions.auth.subscribeToAuthChanges);
+
+  useEffect(() => {
+    return subscribeToAuthChanges()
+  }, [subscribeToAuthChanges])
+
   return (
     <>
-      
-        <Routes>
-          <Route path='/Login' element={<Login />} />
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path='UserLists' element={
-              <RequireAuth>
-                <UserLists />
-              </RequireAuth>
-            } />
-            <Route path='List/:id' element={
-              <RequireAuth>
-                <ListContents />
-              </RequireAuth>
-            } />
-          </Route>
-          
-        </Routes>
-      
+
+      <Routes>
+        <Route path='/Login' element={<Login />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='UserLists' element={
+            <RequireAuth>
+              <UserLists />
+            </RequireAuth>
+          } />
+          <Route path='List/:id' element={
+            <RequireAuth>
+              <ListContents />
+            </RequireAuth>
+          } />
+        </Route>
+      </Routes>
+
     </>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,43 @@
 import { createStore, action, thunk } from 'easy-peasy'
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { auth } from '../firebase';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged, browserLocalPersistence,
+    browserSessionPersistence, setPersistence } from 'firebase/auth'
+
 
 
 const model = {
     auth: {
         user: null,
+
         setUser: action((state, user) => {
             state.user = user;
         }),
+
+        subscribeToAuthChanges: thunk((actions)=>{
+            return onAuthStateChanged(auth, (user)=>{
+                actions.setUser(user);
+            })
+        }),
+        
         login: thunk(async (actions, payload) => {            
             return signInWithEmailAndPassword(auth, payload.email, payload.password)
                 .then((response) => {
-                    actions.setUser(response.user);
-                    return (response.user);
+                    if(response.user){
+                        let persistence = payload.rememberMe? browserLocalPersistence : browserSessionPersistence;
+                        setPersistence(auth,persistence)
+                        return response.user
+                    }
                 })
-                .catch(() => {
+                .catch((error) => {
+                    console.log(error)
                     return null;
                 })
-
-
         }),
+
         logout: thunk(async (actions) => {
-            return signOut(auth)
-                .then(() => {
-                    actions.setUser(null);
-                })
+            return signOut(auth)                
         })
+
     }
 }
 
