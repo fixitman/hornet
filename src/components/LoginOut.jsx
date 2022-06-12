6922b763aad50a1a538733198f7fad106147fe3f
mcollins <fixitman.mike@gmail.com>
2022-06-11 20:41:16 -0400

    modified UI to use new user profile

--- src/components/LoginOut.jsx
+++ src/components/LoginOut.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from '@mui/material';
+import { Fab, useTheme } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStoreActions, useStoreState } from 'easy-peasy';
@@ -13,6 +13,7 @@ const LoginOut = () => {
     const logout = useStoreActions(actions =>  actions.auth.logout );
     const theme = useTheme();
     const user = useStoreState(state => state.auth.user);
+    const initials = user ? user.firstName.slice(0,1) + user.lastName.slice(0,1) : ''
     const buttonStyle = {
         color: theme.palette.common.white,
         textAlign: 'start',
@@ -20,12 +21,13 @@ const LoginOut = () => {
     }
 
 
+
     const navigate = useNavigate()
 
     return (
         <>
             {!user && <Button size='large' onClick={() => navigate('/Login', { state: { from: '/' } })} sx={{ ...buttonStyle }}>Login</Button>}
-            {user && <Button size='large' onClick={logout} sx={{ ...buttonStyle }}>Logout</Button>}
+            {user && <><Fab color="secondary" size='small'>{initials}</Fab><Button size='large' onClick={logout} sx={{ ...buttonStyle }}>Logout</Button></>}
         </>
     );
 
