edb0a949f8b03705e22dfc1c220f5172ca0268fe
mcollins <fixitman.mike@gmail.com>
2022-07-01 01:16:07 -0400

    cleaned up firebase initialization code

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,16 @@
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
 import firebaseConfig from "./secret/firebaseConfig";
 
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
+const db = getFirestore(app);
+
+//use emulator
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   connectAuthEmulator(auth, "http://localhost:9099");
-}
-
-
-const db = getFirestore(app);
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   connectFirestoreEmulator(db, 'localhost', 8080, { ssl: false });
 }
 
